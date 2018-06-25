001608cc3ae648d4f6307f459b229e8fa169a4a1
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-06-25 05:38:49 -0500

    fix

--- FrontEnd/src/app/common/data.service.ts
+++ FrontEnd/src/app/common/data.service.ts
@@ -9,25 +9,25 @@ export class DataService {
   constructor(private http:HttpClient) { }
 
 // // *** local host****/
-  // getUserDetail()
-  // {
-  //   // return this.http.get("http://localhost:3000/getCarLast")
-  //   return this.http.get("http://localhost:3000/getCarLast")
-  // }
-  // addUser(item){
-  //   return this.http.post("http://localhost:3000/saveCar",item)
-  //   // return this.http.post("http://localhost:3000/saveCar",item)
-  // }
+  getUserDetail()
+  {
+    // return this.http.get("http://localhost:3000/getCarLast")
+    return this.http.get("http://localhost:3000/getCarLast")
+  }
+  addUser(item){
+    return this.http.post("http://localhost:3000/saveCar",item)
+    // return this.http.post("http://localhost:3000/saveCar",item)
+  }
 
   // // deployed heroku server
 
-  getUserDetail()
-  {
-    return this.http.get("https://usedcarsexam.herokuapp.com/getCarLast")
-  }
-  addUser(item){
-    return this.http.post("https://usedcarsexam.herokuapp.com/saveCar",item)
-  }
+  // getUserDetail()
+  // {
+  //   return this.http.get("https://usedcarsexam.herokuapp.com/getCarLast")
+  // }
+  // addUser(item){
+  //   return this.http.post("https://usedcarsexam.herokuapp.com/saveCar",item)
+  // }
 
 }
 
