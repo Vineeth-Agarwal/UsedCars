4eb7aef547bb9cbef03ca778bb75d6710444422c
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-06-25 05:24:20 -0500

    heroku

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 { path : '', redirectTo: "/carDetails",pathMatch: 'full'}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- app.js
+++ app.js
@@ -11,14 +11,15 @@ require("./config/database");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'FrontEnd/dist/usedCarsfront'));
+app.engine("html",require("ejs").renderFile);
+app.set('view engine', 'html');
 app.use( cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'FrontEnd/dist/usedCarsfront')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
