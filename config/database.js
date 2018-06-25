434980e60cc9fef33af92f79db32c26d987c059b
Vineeth Agarwal <vineeth.agarwal06@gmail.com>
2018-06-25 11:02:53 -0500

    realign

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 // module.exports = mongoose.connect("mongodb+srv://admin:admin123@cluster0-5iafy.mongodb.net/car?retryWrites=true"
-module.exports = mongoose.connect("mongodb+srv://vineeth:fAzlIxXTBGrEHUBD@cluster0-zlyhn.mongodb.net/UsedCars?retryWrites=true"
+module.exports = mongoose.connect("mongodb+srv://vineeth:fAzlIxXTBGrEHUBD@cluster0-zlyhn.mongodb.net/UsedCars1?retryWrites=true"
     , success => {
         console.log("DB Connected")
     }, err => {
